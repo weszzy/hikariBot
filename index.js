422278b2f097854931a9c02d3c640a4facb4ca1e
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2024-08-12 14:43:26 -0300

    added new authors

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const cron = require('node-cron');
 const { format } = require('date-fns');
 const { ptBR } = require('date-fns/locale');
 
-// Carregar os versículos bíblicos do arquivo JSON
-const biblicalQuotes = JSON.parse(fs.readFileSync(path.join(__dirname, 'biblicalQuotes.json'), 'utf-8')).quotes;
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 const discordChannelId = process.env.DISCORD_CHANNEL_ID;
@@ -33,10 +30,17 @@ const categories = {
     "Cristãos": {
         "Ellen G. White": "https://drive.google.com/drive/folders/1-iQzjDK3Acv4DuBeEJc0DMWrbCM0Vgd3?usp=drive_link",
         "C. S. Lewis": "https://drive.google.com/drive/folders/1-4Nd3TmNOC-dPYY0YVm0AZL4rEKDOiAo?usp=drive_link",
-        "Augusto Cury": "https://drive.google.com/drive/folders/15xDrTy8u7mfEn9Rc-IrwsaewsfBUMZTw?usp=drive_link"
+        "Augusto Cury": "https://drive.google.com/drive/folders/15xDrTy8u7mfEn9Rc-IrwsaewsfBUMZTw?usp=drive_link",
+        "Rick Warren": "https://drive.google.com/drive/folders/17nABD8m3tRtM_Ob5pb6b1SGUWnm_U8cp?usp=drive_link"
+    },
+    "Brasileiros": {
+        "Carla Madeira": "https://drive.google.com/drive/folders/1yISsfinXj8ZZc4l3sxZQMt6wIdp6fC02?usp=drive_link",
+        "Jose de Alencar": "https://drive.google.com/drive/folders/1YCfkeGbGsTdBloYmufM2YZgenHZ768QE?usp=drive_link"
     },
     "Outros": {
-        "Colleen Hoover": "https://drive.google.com/drive/folders/your-folder-id-here"
+        "Colleen Hoover": "https://drive.google.com/drive/folders/17PRzxNa4y5Hb9jB_YZ2m0tnhKd5EptwN?usp=drive_link",
+        "John Green": "https://drive.google.com/drive/folders/1AKb3kkG_6D6gEi8T1YFqA7WZ9cmkogG6?usp=drive_link",
+        "Sarah J. Maas": "https://drive.google.com/drive/folders/18nsKzDMePQ2GNSyHFwN7JXqeQhPpKF--?usp=drive_link"
     }
 };
 
