7cea6dd72a63957006d7531d6cc64e99b2db6d45
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2024-08-12 18:27:49 -0300

    file to define commands

---
+++ config/commands.js
@@ -0,0 +1,16 @@
+const commands = [
+    {
+        name: 'lidos',
+        description: 'Mostra os livros já lidos.',
+    },
+    {
+        name: 'livroatual',
+        description: 'Envia um resumo do livro que estamos lendo.',
+    },
+    {
+        name: 'biblioteca',
+        description: 'Mostra uma lista de categorias de livros disponíveis',
+    },
+];
+
+module.exports = { commands };
