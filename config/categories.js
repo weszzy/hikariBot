632cb32f23140078f36a2848fbd5cdb9b9dfb8d2
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2024-08-15 17:44:53 -0300

    new authors added

--- config/categories.js
+++ config/categories.js
@@ -1,19 +1,27 @@
 const categories = {
     "Cristãos": {
+        "Augusto Cury": "https://drive.google.com/drive/folders/15xDrTy8u7mfEn9Rc-IrwsaewsfBUMZTw?usp=drive_link",
         "Ellen G. White": "https://drive.google.com/drive/folders/1-iQzjDK3Acv4DuBeEJc0DMWrbCM0Vgd3?usp=drive_link",
         "C. S. Lewis": "https://drive.google.com/drive/folders/1-4Nd3TmNOC-dPYY0YVm0AZL4rEKDOiAo?usp=drive_link",
-        "Augusto Cury": "https://drive.google.com/drive/folders/15xDrTy8u7mfEn9Rc-IrwsaewsfBUMZTw?usp=drive_link",
+        "Michelson Borges": "https://drive.google.com/drive/u/3/folders/1Auokmu_yJh8l1Nwgl2hTAKDUx5h_IXMV",
         "Rick Warren": "https://drive.google.com/drive/folders/17nABD8m3tRtM_Ob5pb6b1SGUWnm_U8cp?usp=drive_link"
+
     },
     "Brasileiros": {
+        "Ana Suy": "https://drive.google.com/drive/u/3/folders/1KO-bNzr5pEBIWHYt_o4wqRd9J53km5I0",
         "Carla Madeira": "https://drive.google.com/drive/folders/1yISsfinXj8ZZc4l3sxZQMt6wIdp6fC02?usp=drive_link",
         "Jose de Alencar": "https://drive.google.com/drive/folders/1YCfkeGbGsTdBloYmufM2YZgenHZ768QE?usp=drive_link"
     },
     "Outros": {
+        "Alyson Noel": "https://drive.google.com/drive/u/3/folders/1KRdLUPB_7010ArAbJpKLicl1VD8EABw3",
         "Colleen Hoover": "https://drive.google.com/drive/folders/17PRzxNa4y5Hb9jB_YZ2m0tnhKd5EptwN?usp=drive_link",
+        "Dolly Alderton": "https://drive.google.com/drive/u/3/folders/15SaZcVttNL0s-M80gmWfZXU0Wth-Z7Wk",
+        "Fiódor Dostoiévski": "https://drive.google.com/drive/folders/18nsKzDMePQ2GNSyHFwN7JXqeQhPpKF--?usp=drive_link",
+        "Holly Black": "https://drive.google.com/drive/u/3/folders/1e3AWnHpMb9VvFN7yMP4-bw8r7tiPRlMR",
         "John Green": "https://drive.google.com/drive/folders/1AKb3kkG_6D6gEi8T1YFqA7WZ9cmkogG6?usp=drive_link",
-        "Sarah J. Maas": "https://drive.google.com/drive/folders/18nsKzDMePQ2GNSyHFwN7JXqeQhPpKF--?usp=drive_link"
+        "Osamu Dazai": "https://drive.google.com/drive/u/3/folders/1GxcS1x5AgjXpYlXCxfKJHukJuyjlqbIs",
+        "Sarah J. Maas": "https://drive.google.com/drive/folders/18nsKzDMePQ2GNSyHFwN7JXqeQhPpKF--?usp=drive_link",
     }
-    };
+};
 
-    module.exports = categories;
+module.exports = categories;
