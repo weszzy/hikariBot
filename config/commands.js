256a8ebac5ea23285ad2126bcecccb987047f29b
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2024-08-16 20:13:11 -0300

    removed /lidos and /livroatual due to many bugs

--- config/commands.js
+++ config/commands.js
@@ -1,16 +1,8 @@
 const commands = [
     {
-        name: 'lidos',
-        description: 'Mostra os livros já lidos.',
-    },
-    {
-        name: 'livroatual',
-        description: 'Envia um resumo do livro que estamos lendo.',
-    },
-    {
         name: 'biblioteca',
         description: 'Mostra uma lista de categorias de livros disponíveis',
-    },
+    }
 ];
 
 module.exports = { commands };
