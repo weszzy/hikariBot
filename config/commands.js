9980a771289e1a5d4d6bbc3d54b683f7f4a75212
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2024-08-18 20:49:36 -0300

    declared the new command for "recommend"

--- config/commands.js
+++ config/commands.js
@@ -2,6 +2,10 @@ const commands = [
     {
         name: 'biblioteca',
         description: 'Mostra uma lista de categorias de livros disponíveis',
+    },
+    {
+        name: 'recomendacao',
+        description: 'Receba uma recomendação de livro semanalmente',
     }
 ];
 
