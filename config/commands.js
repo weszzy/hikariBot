d46545023833e6188e64dec52c0c0f3e0a77b121
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2024-08-22 16:11:30 -0300

    added new commands

--- config/commands.js
+++ config/commands.js
@@ -5,7 +5,43 @@ const commands = [
     },
     {
         name: 'recomendacao',
-        description: 'Receba uma recomendação aleatória',
+        description: 'Enviamos um livro aleatório',
+    },
+    {
+        name: 'autor',
+        description: 'Comando para obter informações sobre um autor',
+        options: [
+            {
+                type: 3, // STRING
+                name: 'nome',
+                description: 'Nome do autor',
+                required: true,
+            },
+        ],
+    },
+    {
+        name: 'biblia',
+        description: 'Busca um versículo da Bíblia',
+        options: [
+            {
+                name: 'livro',
+                type: 3, // STRING
+                description: 'O livro da Bíblia (ex: joão)',
+                required: true,
+            },
+            {
+                name: 'capitulo',
+                type: 4, // INTEGER
+                description: 'O capítulo da Bíblia',
+                required: true,
+            },
+            {
+                name: 'versiculo',
+                type: 4, // INTEGER
+                description: 'O versículo da Bíblia',
+                required: true,
+            },
+        ],
     }
 ];
 
